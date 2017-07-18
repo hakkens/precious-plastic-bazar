a42a9ec82b87e5f59875a7158d88fb22197ce241
Daniel Ladwig <daniel.ladwig@hotmail.de>
2017-07-18 14:28:37 +0200

    Update javascript.js

--- Sharetribe/javascript.js
+++ Sharetribe/javascript.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){ 
-  var toggleHeader = document.querySelector(".home-toolbar .toggle-header"); console.log(toggleHeader.textContent)
+  var toggleHeader = document.querySelector(".home-toolbar .toggle-header");
+  console.log(toggleHeader.textContent)
+  if (toggleHeader.textContent === "All listing types") {
   toggleHeader.textContent = "Buy & request";
-
+  }
   console.log(toggleHeader.textContent)
 })
